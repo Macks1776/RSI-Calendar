c9d874993dbf225c0470cfcb6f078490cccfcebf
Max Swann <max@gmail.com>
2021-09-12 13:03:32 -0400

    Created the Views and Controller for the User Calendar (the main view of the app)

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSI_Calendar.Controllers
+{
+    public class CalendarController : Controller
+    {
+
+        public IActionResult Calendar()
+        {
+            return View("Calendar");
+        }
+
+        public IActionResult Day()
+        {
+            return View("Day");
+        }
+        
+        public IActionResult MonthList()
+        {
+            return View("MonthList");
+        }
+
+    }
+}
