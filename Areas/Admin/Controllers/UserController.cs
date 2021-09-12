ce7c7b517b147b77abb9e03dc6057d12cf1f018c
Max Swann <max@gmail.com>
2021-09-12 13:10:27 -0400

    The Admin area of the app. This is where the admin only controlls will live, such as creating and deleting users. There is also a view all and search view to make managing easier.

---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSI_Calendar.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View("Add");
+        }
+
+        public IActionResult Delete()
+        {
+            return View("Delete");
+        }
+
+        public IActionResult Search()
+        {
+            return View("Search");
+        }
+
+        public IActionResult ViewUsers()
+        {
+            return View("ViewUsers");
+        }
+    }
+}
