f5feab59e475f334e6e0d8c30818ae0ce78836b1
Max Swann <max@gmail.com>
2021-09-12 13:14:00 -0400

    The Cultrual Ambassador Area for the app. I named it CulAm to be easier to type. This is where Cultural Ambassador only functions will live, such as adding, deleting and editing views. I added a View All view because I figured instead of the Cultural Ambassador viewing the events the exact same way as users, it would be nice if they had a way to just view the events table, sorted by day. We can still provide buttons that allow them to go and have the same views as a regular employee.

---
+++ Areas/CulAm/Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSI_Calendar.Areas.CulAm.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Edit()
+        {
+            return View("Edit");
+        }
+
+        public IActionResult Delete()
+        {
+            return View("Delete");
+        }
+
+        public IActionResult ViewAll()
+        {
+            return View("ViewAll");
+        }
+    }
+}
