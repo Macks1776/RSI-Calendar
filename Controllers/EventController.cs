85e70002f6363dac55a9bdcb30008faa1c7cda5f
Max Swann <max@gmail.com>
2021-09-12 13:05:33 -0400

    Created the Event Controller and Views for the User Area (The main view of the app)

---
+++ Controllers/EventController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSI_Calendar.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Details()
+        {
+            return View("Details");
+        }
+
+        public IActionResult Search()
+        {
+            return View("Details");
+        }
+    }
+}
