1a13d6147b7864bd405eecd347faf0c96b70312d
L. Andrew Overholt <65783599+aOverholt@users.noreply.github.com>
2021-10-03 23:49:16 -0400

    Renamed "Areas/Admin/Views/User/Add" to "Areas/Admin/Views/User/Edit"

--- Areas/Admin/Controllers/UserController.cs
+++ Areas/Admin/Controllers/UserController.cs
@@ -8,9 +8,9 @@ namespace RSI_Calendar.Areas.Admin.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Add()
+        public IActionResult Edit()
         {
-            return View("Add");
+            return View("Edit");
         }
 
         public IActionResult Delete()
