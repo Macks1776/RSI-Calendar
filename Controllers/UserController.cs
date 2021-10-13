31361e6d700690829e7904acd65214c14f3b16ae
L. Andrew Overholt <loverhol@smartweb.augustatech.edu>
2021-10-12 20:47:13 -0400

    - Made loading of user settings information dynamic

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,16 +3,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RSI_Calendar.Models;
 
 namespace RSI_Calendar.Controllers
 {
     public class UserController : Controller
     {
+        private CalendarContext context;
+
+        public UserController(CalendarContext ctx)
+        {
+            context = ctx;
+        }
+
+
         public IActionResult LogIn()
         {
             return View("LogIn");
         }
 
+        [HttpGet]
+        public IActionResult Settings(int id = 1)
+        {
+            Employee employee = context.Employees.Find(id);
+
+            return View(employee);
+        }
+
+        [HttpPost]
         public IActionResult Settings()
         {
             return View("Settings");
