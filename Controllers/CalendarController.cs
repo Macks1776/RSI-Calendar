a977a6b12f8b2ff73555d207bee96e3a0a9dc3f8
Max Swann <max@gmail.com>
2021-10-21 00:04:38 -0400

    Color coded the calendar by passing each type of event to a different json and then in the calendar event sources each json is given different color settings.

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -42,6 +42,70 @@ namespace RSI_Calendar.Controllers
 
             return json;
         }
+
+        public string GetRequiredEvents()
+        {
+            var requiredEvents = context.Events.Where(e => e.Type == "Req").ToList();
+            List<CalendarEvent> requiredEventList = new List<CalendarEvent>();
+
+            foreach (var data in requiredEvents)
+            {
+                CalendarEvent calevent = new CalendarEvent(data.EventID, data.Name, data.StartDate, data.EndDate, data.Description);
+                requiredEventList.Add(calevent);
+            }
+
+            string jsonRequiredEvents = JsonConvert.SerializeObject(requiredEventList);
+
+            return jsonRequiredEvents;
+        }
+
+        public string GetEducationEvents()
+        {
+            var educationEvents = context.Events.Where(e => e.Type == "Edu").ToList();
+            List<CalendarEvent> educationEventList = new List<CalendarEvent>();
+
+            foreach (var data in educationEvents)
+            {
+                CalendarEvent calevent = new CalendarEvent(data.EventID, data.Name, data.StartDate, data.EndDate, data.Description);
+                educationEventList.Add(calevent);
+            }
+
+            string jsonEducationEvents = JsonConvert.SerializeObject(educationEventList);
+
+            return jsonEducationEvents;
+        }
+
+        public string GetFamilyEvents()
+        {
+            var familyEvents = context.Events.Where(e => e.Type == "Fam").ToList();
+            List<CalendarEvent> familyEventList = new List<CalendarEvent>();
+
+            foreach (var data in familyEvents)
+            {
+                CalendarEvent calevent = new CalendarEvent(data.EventID, data.Name, data.StartDate, data.EndDate, data.Description);
+                familyEventList.Add(calevent);
+            }
+
+            string jsonFamilyEvents = JsonConvert.SerializeObject(familyEventList);
+
+            return jsonFamilyEvents;
+        }
+
+        public string GetFunEvents()
+        {
+            var funEvents = context.Events.Where(e => e.Type == "Fun").ToList();
+            List<CalendarEvent> funEventList = new List<CalendarEvent>();
+
+            foreach (var data in funEvents)
+            {
+                CalendarEvent calevent = new CalendarEvent(data.EventID, data.Name, data.StartDate, data.EndDate, data.Description);
+                funEventList.Add(calevent);
+            }
+
+            string jsonFamilyEvents = JsonConvert.SerializeObject(funEventList);
+
+            return jsonFamilyEvents;
+        }
         
     }
 }
