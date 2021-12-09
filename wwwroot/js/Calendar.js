bfeb2ef4aaad061392ab54823830dd6ca0be7f8f
Macks1776 <macks1776@gmail.com>
2021-12-09 11:54:56 -0500

    Removed the move year buttons.

--- wwwroot/js/Calendar.js
+++ wwwroot/js/Calendar.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
             end: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
         },
         footerToolbar: {
-            start: 'prevYear,prev,next,nextYear today',
+            start: 'prev,next today',
             end: 'branchPicker dateJumper'
         },
         eventClick: function (info) {
@@ -88,83 +88,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 
-    document.getElementById('SubmitBranch').addEventListener('click', function () {
-        let branchVal = $("#BranchPicker option:selected").val();
-        BranchPicker.hide();
-
-        calendar = new FullCalendar.Calendar(calendarEl, {
-            eventSources: [
-                {
-                    url: '/calendar/ReqEventJson',
-                    extraParams: {
-                        branch: branchVal
-                    },
-                    color: 'red',
-                    textColor: 'white'
-                },
-                {
-                    url: '/calendar/EduEventJson',
-                    extraParams: {
-                        branch: branchVal
-                    },
-                    color: 'blue',
-                    textColor: 'white'
-                },
-                {
-                    url: '/calendar/FunEventJson',
-                    extraParams: {
-                        branch: branchVal
-                    },
-                    color: 'green',
-                    textColor: 'white'
-                },
-                {
-                    url: '/calendar/FamEventJson',
-                    extraParams: {
-                        branch: branchVal
-                    },
-                    color: 'yellow',
-                    textColor: 'black'
-                }
-            ],
-            initalView: 'dayGridMonth',
-            navLinks: true,
-            height: "auto",
-            headerToolbar: {
-                start: 'title',
-                end: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-            },
-            footerToolbar: {
-                start: 'prevYear,prev,next,nextYear today',
-                end: 'branchPicker dateJumper'
-            },
-            eventClick: function (info) {
-                var eventObj = info.event;
-                if (eventObj.url) {
-                    window.open(eventObj.url);
-                    info.jsEvent.preventDefault();
-                } else {
-                    alert('Clicked Event: ' + eventObj.title);
-                }
-            },
-            customButtons: {
-                dateJumper: {
-                    text: 'Go To Date',
-                    click: function () {
-                        DateJumper.show();
-                    }
-                },
-                branchPicker: {
-                    text: 'Change Branch',
-                    click: function () {
-                        BranchPicker.show();
-                    }
-                }
-            }
-        });
-
-        calendar.refetchEvents();
-    });
+    
 });
 
 
